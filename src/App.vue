15dbbd5d524c628c4832360e7bdfdbcfcdd30f2b
lishaguoguo <2026838933@qq.com>
2017-12-09 17:25:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <vheader></vheader> -->
+    <div class="tab"></div>
+    <div class="content"></div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import vheader from './components/header/header.vue'
+export default {
+  components: {
+    vheader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
